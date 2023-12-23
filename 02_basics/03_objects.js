b36c6794fee7aa331a2ab71206eefa647545a9af
suman kumar <suman.kumar1.mec22@itbhu.ac.in>
2023-12-24 02:05:03 +0530

    objects and array

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,39 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+// acess karna h objrct
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);    // same object m jab referace krte h tab this ka use krte h
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
